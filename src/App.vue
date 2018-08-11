1262366202b42cf1da6ba43a57ee8ef081ee9226
jsonjiaozhu <872080721@qq.com>
2018-08-11 17:32:46 +0800

    添加基本css
    Signed-off-by: jsonjiaozhu <872080721@qq.com>

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,8 @@
 </template>
 
 <script>
+  import './static/css/reset.css';
+  import  './static/css/common.css';
 export default {
   name: 'App'
 }
@@ -18,6 +20,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
